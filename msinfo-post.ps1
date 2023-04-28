fcf6ae6299283a4767b3e123090ca06f348b064c
ds2600 <bill@ishiganto.com>
2023-04-28 16:09:01 -0400

    Initial Commit

---
+++ msinfo-post.ps1
@@ -0,0 +1,13 @@
+﻿Get-Content "C:\msinfo-post\settings.ini" | foreach-object -begin {$h=@{}} -process { $k = [regex]::split($_,'='); if(($k[0].CompareTo("") -ne 0) -and ($k[0].StartsWith("[") -ne $True)) { $h.Add($k[0], $k[1]) } } 
+$COMP_INFO = Get-ComputerInfo
+$UUID = (wmic csproduct get uuid)[2]
+
+$ADDITIONAL_PAR = @{
+    uuid = $UUID
+}
+
+$COMP_INFO | Add-Member $ADDITIONAL_PAR
+$BODY = $COMP_INFO | ConvertTo-Json
+
+#Write-Output $BODY
+$RESP = Invoke-WebRequest -Method Post -Uri $h.Get_Item("API_URI") -Body $BODY -UseBasicParsing
