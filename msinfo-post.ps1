90dabb3b78861ef6a90f5ee1e60bb67e608a1607
ds2600 <bill@ishiganto.com>
2023-05-01 13:31:33 -0400

    Added info requests when running

--- msinfo-post.ps1
+++ msinfo-post.ps1
@@ -1,9 +1,13 @@
-﻿Get-Content "C:\msinfo-post\settings.ini" | foreach-object -begin {$h=@{}} -process { $k = [regex]::split($_,'='); if(($k[0].CompareTo("") -ne 0) -and ($k[0].StartsWith("[") -ne $True)) { $h.Add($k[0], $k[1]) } } 
+﻿$REQUESTOR = Read-Host -Prompt 'Enter your name:'
+$PC_NAME = Read-Host -Prompt 'Enter PC Name:'
+Get-Content "C:\msinfo-post\settings.ini" | foreach-object -begin {$h=@{}} -process { $k = [regex]::split($_,'='); if(($k[0].CompareTo("") -ne 0) -and ($k[0].StartsWith("[") -ne $True)) { $h.Add($k[0], $k[1]) } } 
 $COMP_INFO = Get-ComputerInfo
 $UUID = (wmic csproduct get uuid)[2]
 
 $ADDITIONAL_PAR = @{
     uuid = $UUID
+    requestor = $REQUESTOR
+    pc = $PC_NAME
 }
 
 $COMP_INFO | Add-Member $ADDITIONAL_PAR
